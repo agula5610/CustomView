6785375bff413e40785bf15c910d16cf6ffe3720
yunlu0831 <237489473@qq.com>
2019-07-09 22:57:53 +0800

    2019-07-09 day2 部分源码

--- app/src/main/java/com/hesc/customview/TextView.java
+++ app/src/main/java/com/hesc/customview/TextView.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,6 +22,7 @@ public class TextView extends View {
     private String mText;
     private int mTextSize = 15;
     private int mTextColor = Color.BLACK;
+    private Paint myPaint;
 
     // 构造函数会在代码里面new的时候调用
     //TextView tv = new TextView(this);
@@ -45,12 +49,20 @@ public class TextView extends View {
         super(context, attrs, defStyleAttr);
         // 获取自定义属性
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.TextView);
-        mText = array.getString(R.styleable.TextView_text);
-        mTextColor = array.getColor(R.styleable.TextView_textColor, mTextColor);
+        mText = array.getString(R.styleable.TextView_ctext);
+        mTextColor = array.getColor(R.styleable.TextView_ctextColor, mTextColor);
         // px sp ?
-        mTextSize = array.getDimensionPixelSize(R.styleable.TextView_textSize, mTextSize);
+        mTextSize = array.getDimensionPixelSize(R.styleable.TextView_ctextSize, sp2px(mTextSize));
         // 回收
         array.recycle();
+        myPaint = new Paint();
+        myPaint.setAntiAlias(true);
+        myPaint.setTextSize(mTextSize);
+        myPaint.setColor(mTextColor);
+    }
+
+    private int sp2px(int sp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, getResources().getDisplayMetrics());
     }
 
     /**
@@ -67,17 +79,38 @@ public class TextView extends View {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
+        // 1、确定的值，不需要计算，给多少就是多少
+        // 2、给的是wrap_content，需要计算
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-        if (widthMode == MeasureSpec.UNSPECIFIED) {
-
+        if (widthMode == MeasureSpec.AT_MOST) {
+            // 计算宽度  与字体的长度和大小有关  用画笔来测量
+            Rect bounds = new Rect();
+            // 获取文本的Rect
+            myPaint.getTextBounds(mText, 0, mText.length(), bounds);
+            widthSize = bounds.width();
         }
+
+        if (heightMode == MeasureSpec.AT_MOST) {
+            // 计算宽度  与字体的长度和大小有关  用画笔来测量
+            Rect bounds = new Rect();
+            // 获取文本的Rect
+            myPaint.getTextBounds(mText, 0, mText.length(), bounds);
+            heightSize = bounds.height();
+        }
+        // 设置控件的宽高
+        setMeasuredDimension(widthSize, heightSize);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        // 画文字  text  x    y   paint
+        // x 就是开始的位置
+        // y 基线  baseline
+        canvas.drawText(mText, 0, getHeight() / 2, myPaint);
+
     }
 
     /**
